43f8c487228469400cbfb6ab8eee6dddd78cb66c
Rani Shah <rani76@gmail.com>
2018-03-01 12:37:57 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,8 +21,8 @@ I accept the following commands:
 end
 
 def list(songs)
-  songs.each_with_index do |song, index|
-        puts "#{index+1}. #{song}"
+  songs.each_with_index do |song, i|
+        puts "#{i+1}. #{song}"
   end
 end
 
